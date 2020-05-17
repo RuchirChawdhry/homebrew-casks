c274e7fbf5c6b4392de9f4b03f927c11f7d6e994
Ruchir Chawdhry <ruchir.github@outlook.com>
2020-05-17 13:51:03 +0530

    NeatDownloadManager cask

---
+++ Casks/neat-download-manager.rb
@@ -0,0 +1,10 @@
+cask 'neat-download-manager' do:
+    version '1.1.10'
+    sha256 '8290631e87c3b5f389e097eadbc17d199db0065d0506e73dde37080ac5930516'
+
+    url 'https://www.neatdownloadmanager.com/file/NeatDMInstaller.dmg'
+    name 'NeatDownloadManager'
+    homepage 'https://www.neatdownloadmanager.com/'
+    
+    app 'NeatDownloadManager.app'
+end
