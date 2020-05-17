aa5acfb8eff901f1938bb7fd7f493bea34d78498
Ruchir Chawdhry <ruchir.github@outlook.com>
2020-05-17 15:12:20 +0530

    DocFetcher cask

--- Casks/docfetcher.rb
+++ Casks/docfetcher.rb
@@ -1,6 +1,6 @@
 cask 'docfetcher' do
     version '1.1.22'
-    sha256 :no_fetch
+    sha256 '658ef7d80cb28d9cd108904db5ad5ec19a158ba5dff063660cdfbdd927567d77'
 
     url 'https://excellmedia.dl.sourceforge.net/project/docfetcher/docfetcher/1.1.22/DocFetcher-1.1.22.dmg'
     appcast 'https://sourceforge.net/projects/docfetcher/rss?path=/docfetcher'
